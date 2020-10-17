dbbf1e5dd798d8b15245d2e6b8930b9adf4abbbd
Joxit <joxit972@gmail.com>
2020-10-17 19:01:28 +0200

    feat(git): improve `get_all_commits_before` and use revwalk

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -20,27 +20,47 @@ impl Git {
   }
 
   pub fn get_all_commits_before(&self, from: &String) -> Vec<Commit> {
-    let repository = self.repository();
+    let repo = self.repository();
+    let mut walk = repo.revwalk().unwrap();
 
-    let start_commit = repository
+    let start_commit = repo
       .revparse_single(&from)
       .unwrap()
       .peel_to_commit()
       .unwrap();
-    let mut result = BTreeSet::new();
-    commit_walk(&start_commit, &mut result);
-    result.into_iter().collect::<Vec<Commit>>()
+
+    walk.push(start_commit.id()).unwrap();
+    walk.set_sorting(git2::Sort::TOPOLOGICAL).unwrap();
+
+    walk
+      .into_iter()
+      .map(|c| c.unwrap())
+      .map(|c| repo.revparse_single(format!("{}", c).as_str()).unwrap())
+      .map(|c| c.peel_to_commit().unwrap())
+      .map(Commit::from)
+      .collect()
   }
 
   pub fn get_all_tags(&self) -> Vec<String> {
     let mut res = vec![];
     // Equivalent to self.repository().references() with ref filter
-    self.repository().tag_foreach(|_, name| {
-      res.push(std::str::from_utf8(name).unwrap().chars().skip(10).collect::<String>());
-      true
-    }).unwrap();
+    self
+      .repository()
+      .tag_foreach(|_, name| {
+        res.push(remove_ref_tags(name));
+        true
+      })
+      .unwrap();
     res
   }
+}
+
+fn remove_ref_tags(name: &[u8]) -> String {
+  std::str::from_utf8(name)
+    .unwrap()
+    .chars()
+    .skip(10)
+    .collect::<String>()
 }
 
 fn commit_walk(commit: &git2::Commit, result: &mut BTreeSet<Commit>) {
